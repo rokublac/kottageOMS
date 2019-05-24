c837719c4218eb9947f4d965659ca44c4f533a83
Roku Blac <33192458+rokublac@users.noreply.github.com>
2019-05-24 21:36:46 +1000

    Data logging module

--- modules/module-dataLogging.js
+++ modules/module-dataLogging.js
@@ -1,7 +1,7 @@
 import wixData from 'wix-data';
 
 
-// log database changes to the data logging database
+// log and insert database activity to the data logging database
 export function dataLogging(oldValue, newValue, logAction, orderId) {
 	let toInsert = {
 		"title": `${oldValue} --> ${newValue}`,
