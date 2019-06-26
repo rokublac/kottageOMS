6644a439aca42228049ca45af3ca71219e075dcb
RB <33192458+rokublac@users.noreply.github.com>
2019-06-27 00:40:44 +1000

    added order creation logging

--- modules/module-dataLogging.js
+++ modules/module-dataLogging.js
@@ -1,11 +1,24 @@
 import wixData from 'wix-data';
 
 
-// log and insert database activity to the data logging database
-export function dataLogging(oldValue, newValue, logAction, orderId) {
+// log database changes to the data logging database (for order updates)
+export function updateDataLogging(oldValue, newValue, logAction, orderId) {
 	let toInsert = {
 		"title": `${oldValue} --> ${newValue}`,
 		"logAction": logAction,
+		"orderId": orderId
+	}
+	wixData.insert('dbLogs', toInsert)
+		.then((res) => {
+			console.log('action logged.');
+		})
+}
+
+// log order creations
+export function createOrderDataLogging(custName, orderId) {
+	let toInsert = {
+		"title": `Order created for ${custName}`,
+		"logAction": 'Order created',
 		"orderId": orderId
 	}
 	wixData.insert('dbLogs', toInsert)
